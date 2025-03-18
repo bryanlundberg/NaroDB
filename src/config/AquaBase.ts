989ed214ce249c96256bde47f71ee9f786571922
Bryan Lundberg <bryanlundberg@outlook.com>
2025-03-17 22:26:03 -0600

    feat: Add database functionality with data management logic
    Introduced `AquaBase` class to handle database operations such as adding, retrieving, updating, and deleting items, along with file persistence. Added support for `lodash` and `@faker-js/faker` for enhanced data handling and test data generation. Updated dependencies and devDependencies in `package.json` to reflect the changes.

--- src/config/AquaBase.ts
+++ src/config/AquaBase.ts
@@ -1,34 +1,97 @@
-import fs from 'fs-extra'
+import fs from 'fs-extra';
+import { ObjectId } from "bson";
+import { findIndex, find } from 'lodash';
 
 export class AquaBase {
-  private rootPath: string;
-  private logFile: string = 'data.json';
+  private rootPath: string = "./data";
+  private readonly dbName: string;
+  private readonly logFileName: string = "data.json";
+  private collections: { [key: string]: any } = {};
 
-  constructor(rootPath: string = './data') {
-    this.rootPath = rootPath;
-    this.initializeRoot()
+  constructor(dbName: string) {
+    if (dbName.includes('/')) throw new Error("dbName cannot contain '/'");
+    this.dbName = dbName;
+    this.initializeDir(this.getPathToDB());
+    this.preloader();
   }
 
-  private initializeRoot() {
-    fs.ensureDirSync(this.rootPath);
-    return;
+  private initializeDir(path: string) {
+    fs.ensureDirSync(path);
   }
 
-  public createDatabase(dbName: string) {
-    const dbPath = `${this.rootPath}/${dbName}`
-    fs.ensureDirSync(dbPath)
-    return this
+  private preloader() {
+    const collectionsDir = this.getPathToDB();
+    fs.readdirSync(collectionsDir).forEach(folderName => {
+      const folderPath = `${collectionsDir}/${folderName}`;
+      if (fs.lstatSync(folderPath).isDirectory()) {
+        const dataPath = `${folderPath}/${this.logFileName}`;
+        if (fs.existsSync(dataPath)) {
+          this.collections[folderName] = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+        } else {
+          this.collections[folderName] = [];
+        }
+      }
+    });
   }
 
-  public createCollection(dbName: string, collectionName: string) {
-    const collectionPath = `${this.rootPath}/${dbName}/${collectionName}`
-    const logFilePath = `${this.rootPath}/${dbName}/${collectionName}/${this.logFile}`
-    fs.ensureFileSync(collectionPath)
+  private getPathToDB() {
+    return `${this.rootPath}/${this.dbName}`;
+  }
 
-    if (!fs.pathExistsSync(logFilePath)) {
-      fs.writeJsonSync(collectionPath, {})
-    }
+  add(collectionName: string, data: any) {
+    const newItem = { ...data, id: new ObjectId().toHexString() };
+    this.collections[collectionName].push(newItem);
+    return structuredClone(newItem);
+  }
+
+  getAll(collectionName: string) {
+    return this.collections[collectionName];
+  }
+
+  get(path: string) {
+    const { collectionName, collectionId } = this.validatePath(path);
+    // TODO: Implement subCollection support
+    return find(this.collections[collectionName] || [], (item: any) => item.id === collectionId) || undefined;
+  }
+
+  private validatePath(path: string) {
+    const pathParts = path.split('/');
+    if (pathParts.length <= 1) throw new Error("Invalid path format. Ensure the path is in the format 'collectionName/id'.");
+    if (pathParts.length === 3 || pathParts.length >= 5) throw new Error("Invalid path format. Ensure the path is in the format 'collectionName/id/subCollectionName/id'.");
+    return {
+      collectionName: pathParts[0],
+      collectionId: pathParts[1],
+      subCollectionName: pathParts[2],
+      subCollectionId: pathParts[3]
+    };
+  }
+
+  update(path: string, data: any) {
+    const { collectionName, collectionId } = this.validatePath(path);
+    const collection = this.collections[collectionName];
+    if (!collection) throw new Error("Collection not found");
+
+    const itemIndex = findIndex(collection, (item: any) => item.id === collectionId);
+    if (itemIndex === -1) throw new Error("Item not found");
+
+    collection[itemIndex] = { ...collection[itemIndex], ...data };
+  }
+
+  delete(path: string) {
+    const { collectionName, collectionId } = this.validatePath(path);
+    const collection = this.collections[collectionName];
+    if (!collection) throw new Error("Collection not found");
+
+    const itemIndex = findIndex(collection, (item: any) => item.id === collectionId);
+    if (itemIndex === -1) throw new Error("Item not found");
+
+    collection.splice(itemIndex, 1);
+  }
+
+  writeToFile() {
+    Object.keys(this.collections).forEach(collectionName => {
+      const dataPath = `${this.getPathToDB()}/${collectionName}/${this.logFileName}`;
+      fs.writeFileSync(dataPath, JSON.stringify(this.collections[collectionName]));
+    });
   }
 }
-
-
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { AquaBase } from "./config/AquaBase.ts";
+import { faker } from "@faker-js/faker/locale/en";
 
-const db = new AquaBase().createDatabase('test')
+const db = new AquaBase("67d89f7d91e2ee84a95e4b0c");
 
-db.createCollection('test', 'users')
+// console.log(db.add("users", { name: faker.word.sample(50), age: faker.number.int() }))
+// console.log(db.getAll("users"));
+console.time("add");
+console.log(db.get("users/67d8f4ab12b23fc77fcafa67"));
+console.log(db.delete("users/67d8f4ab12b23fc77fcafa67"));
+console.log(db.get("users/67d8f4ab12b23fc77fcafa67"));
+for (let i = 0; i < 1000000; i++) {
+  db.add("users", { name: faker.word.sample(50), age: faker.number.int() });
+}
+
+db.writeToFile()
+
+console.timeEnd("add");
