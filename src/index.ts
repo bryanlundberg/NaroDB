3a510a637796e8f0e2d3b394b66878bfb4dcf7a6
Bryan Lundberg <bryanlundberg@outlook.com>
2025-03-19 07:56:56 -0600

    playground experiments
    Removed excessive debug code and redundant function calls to streamline the file. Retained key functions for testing purposes while reducing clutter for improved readability.

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ import { faker } from "@faker-js/faker/locale/en";
 
 const db = new AquaBase("67d89f7d91e2ee84a95e4b0c");
 
+// console.log(db.getAll("users"));
+console.time("test");
+// const id = "67da1dd162d2360c1b5999c2"
+// console.log(db.get("users/"+id));
+// console.log(db.delete("users/"+id));
+// console.log(db.get("users/"+id));
 // console.log(db.add("users", { name: faker.word.sample(50), age: faker.number.int() }))
 // console.log(db.getAll("users"));
-console.time("add");
-console.log(db.get("users/67d8f4ab12b23fc77fcafa67"));
-console.log(db.delete("users/67d8f4ab12b23fc77fcafa67"));
-console.log(db.get("users/67d8f4ab12b23fc77fcafa67"));
-for (let i = 0; i < 1000000; i++) {
-  db.add("users", { name: faker.word.sample(50), age: faker.number.int() });
-}
+// db.delete("users/67dac46096caf856607aac67");
+// console.time("write");
+// db.writeToFile()
+// console.timeEnd("write");
+console.timeEnd("test");
 
-db.writeToFile()
 
-console.timeEnd("add");
+console.log(Date.now().toString(36) + Math.random().toString(36).substr(2, 9))
