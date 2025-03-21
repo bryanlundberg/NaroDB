a4b4b1e912877e1b55779c3952fae7f39c451120
Bryan Lundberg <bryanlundberg@outlook.com>
2025-03-21 08:42:34 -0600

    refactor: improve NaroPath validation and add unit tests
    refactored path validation logic to enhance clarity and consistency. added comprehensive unit tests to cover edge cases and ensure proper validation of path formats.

---
+++ __test__/unit/NaroPath/NaroPath.test.ts
@@ -0,0 +1,65 @@
+import { describe, expect, test } from "vitest";
+import { NaroPath } from "../../../src/config/NaroPath";
+import { COLLECTION_NAME, SUB_COLLECTION_NAME } from "../../constants";
+
+describe("NaroPath, should validate paths are well structured", () => {
+  test("Should throw error if path is empty", () => {
+    expect(() => NaroPath.validate("")).toThrowError();
+  })
+
+  test("Should accept 'collectionName' as path ", () => {
+    const { collectionName, collectionId, subCollectionName, subCollectionId } = NaroPath.validate(COLLECTION_NAME);
+    expect(collectionName).toBe(COLLECTION_NAME);
+    expect(collectionId).toBeUndefined();
+    expect(subCollectionName).toBeUndefined();
+    expect(subCollectionId).toBeUndefined();
+  });
+
+  test("Should accept 'collectionName/id' as path ", () => {
+    const { collectionName, collectionId, subCollectionName, subCollectionId } = NaroPath.validate(`${COLLECTION_NAME}/1234`);
+    expect(collectionName).toBe(COLLECTION_NAME);
+    expect(collectionId).toBe("1234");
+    expect(subCollectionName).toBeUndefined();
+    expect(subCollectionId).toBeUndefined();
+  });
+
+  test("Should accept 'collectionName/id/subCollectionName' as path ", () => {
+    const { collectionName, collectionId, subCollectionName, subCollectionId } = NaroPath.validate(`${COLLECTION_NAME}/1234/${SUB_COLLECTION_NAME}`);
+    expect(collectionName).toBe(COLLECTION_NAME);
+    expect(collectionId).toBe("1234");
+    expect(subCollectionName).toBe(SUB_COLLECTION_NAME);
+    expect(subCollectionId).toBeUndefined();
+  });
+
+  test("Should accept 'collectionName/id/subCollectionName/id' as path ", () => {
+    const { collectionName, collectionId, subCollectionName, subCollectionId } = NaroPath.validate(`${COLLECTION_NAME}/1234/${SUB_COLLECTION_NAME}/5678`);
+    expect(collectionName).toBe(COLLECTION_NAME);
+    expect(collectionId).toBe("1234");
+    expect(subCollectionName).toBe(SUB_COLLECTION_NAME);
+    expect(subCollectionId).toBe("5678");
+  });
+
+  test("Should throw error if path is not well structured collection/id/sub-collection/id/extra", () => {
+    expect(() => NaroPath.validate(`${COLLECTION_NAME}/1234/${SUB_COLLECTION_NAME}/5678/extra`)).toThrowError();
+  });
+
+  test("Should throw error if path is not well structured collection/id/sub-collection/id/extra/sub", () => {
+    expect(() => NaroPath.validate(`${COLLECTION_NAME}/1234/${SUB_COLLECTION_NAME}/5678/extra/sub`)).toThrowError();
+  });
+
+  test("Should throw error if path ends with / as collection/id/sub-collection/id/", () => {
+    expect(() => NaroPath.validate(`${COLLECTION_NAME}/1234/${SUB_COLLECTION_NAME}/5678/`)).toThrowError();
+  });
+
+  test("Should throw error if path ends with / as collection/id/sub-collection/", () => {
+    expect(() => NaroPath.validate(`${COLLECTION_NAME}/1234/${SUB_COLLECTION_NAME}/5678/`)).toThrowError();
+  });
+
+  test("Should throw error if path ends with / as collection/id/", () => {
+    expect(() => NaroPath.validate(`${COLLECTION_NAME}/1234/${SUB_COLLECTION_NAME}/5678/`)).toThrowError();
+  });
+
+  test("Should throw error if path ends with / as collection/", () => {
+    expect(() => NaroPath.validate(`${COLLECTION_NAME}/1234/${SUB_COLLECTION_NAME}/5678/`)).toThrowError();
+  });
+});
--- src/config/NaroPath.ts
+++ src/config/NaroPath.ts
@@ -5,13 +5,11 @@ export class NaroPath {
     subCollectionName?: string;
     subCollectionId?: string;
   } {
+    if (!path) throw new Error("Empty path provided.");
     const pathParts = path.split('/');
-    if (pathParts.length <= 1) {
-      throw new Error("Invalid path format. Ensure the path is in the format 'collectionName/id'.");
-    }
-    if (pathParts.length === 3 || pathParts.length >= 5) {
-      throw new Error("Invalid path format. Ensure the path is in the format 'collectionName/id/subCollectionName/id'.");
-    }
+    if (pathParts.length < 1) throw new Error("Invalid path format. Ensure the path is in the format 'collectionName/id'.");
+    if (pathParts.length > 4) throw new Error("Invalid path format. Ensure the path is in the format 'collectionName/id/subCollectionName/id'.");
+
     return {
       collectionName: pathParts[0],
       collectionId: pathParts[1],
