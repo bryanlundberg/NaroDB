79eb2f94bd8689a4c53bd9b85812fe0edb8df01e
Bryan Lundberg <bryanlundberg@outlook.com>
2025-03-24 06:40:12 -0600

    feat: Export NaroId from IdGenerator utility module
    Added NaroId to the exports in index.ts to make it available for external use. This enhances the usability of the IdGenerator utility in the codebase.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Naro } from "./base/Naro";
 export { NaroPath } from "./manage/paths/NaroPath";
 export { NaroFiler } from "./manage/files/NaroFiler";
+export { NaroId } from "./utils/IdGenerator";
