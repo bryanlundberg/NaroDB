f41e3e374e55bb1d2bf48ba146daa6f571b8c14c
Bryan Lundberg <bryanlundberg@outlook.com>
2025-03-30 21:28:44 -0600

    test: Add comprehensive unit tests for Naro database methods
    Expanded test coverage for `get`, `getAll`, and `update` methods in the `Naro` database class. Includes cases for empty collections, non-existent documents, and successful operations, improving reliability and accuracy of the test suite.

--- __test__/unit/base/Naro.test.ts
+++ __test__/unit/base/Naro.test.ts
@@ -10,10 +10,10 @@ describe("Naro", () => {
     const newUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
     expect(newUser).toHaveProperty("id");
   })
-  test("getAll, should return all documents in the users collection (empty array)", async () => {
+  test("getAll, should return an empty array if collection is empty", async () => {
     const db = new Naro(root);
     const users = db.getAll("users");
-    console.log(users);
+    expect(users).toHaveLength(0);
   })
   test("getAll, should return all documents in the users collection (with data)", async () => {
     const db = new Naro(root);
@@ -22,5 +22,30 @@ describe("Naro", () => {
     }
     const users = db.getAll("users");
     expect(users).toHaveLength(5);
+  })
+  test("get, should return a document from the users collection", async () => {
+    const db = new Naro(root);
+    const newUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+    const user = await db.get(`users/${newUser.id}`);
+    expect(user).toHaveProperty("id");
+  })
+  test("get, should return undefined if document is not found", async () => {
+    const db = new Naro(root);
+    const user = await db.get("users/123");
+    expect(user).toBeUndefined();
+  })
+  test("update, should update a document in the users collection", async () => {
+    const db = new Naro(root);
+    const initialUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+    const afterUser = await db.update(`users/${initialUser.id}`, { name: "John Doe" });
+    expect(afterUser).toHaveProperty("name", "John Doe");
+  })
+  test("update, should throw an error if document is not found", async () => {
+    const db = new Naro(root);
+    try {
+      await db.update("users/123", { name: "John Doe" });
+    } catch (error) {
+      expect(error).toHaveProperty("message", "Item not found");
+    }
   })
 });
