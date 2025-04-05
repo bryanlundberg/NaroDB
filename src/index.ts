a85deb43f74fae6e4412374c3e4e63e70bebae28
Bryan Lundberg <bryanlundberg@outlook.com>
2025-04-05 10:54:45 -0600

    feat: Update exports to include new types and classes
    Added export for `NaroDocument` type and reorganized exports for clarity. Removed unused or redundant exports to streamline the codebase.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+/** classes **/
 export { Naro } from "./base/Naro";
-export { NaroPath } from "./manage/paths/NaroPath";
-export { NaroFiler } from "./manage/files/NaroFiler";
 export { NaroId } from "./utils/IdGenerator";
+
+/** Types **/
+export { NaroDocument } from "./base/interfaces/NaroDocument";
