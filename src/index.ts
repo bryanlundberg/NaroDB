f49c09fb3fb5f1d5f5705d2b24300e79bce58ff0
Bryan Lundberg <bryanlundberg@outlook.com>
2025-04-08 06:37:54 -0600

    refactor: Remove unused export for NaroId
    The export for NaroId was removed as it is no longer required. This helps keep the codebase clean and eliminates unnecessary exports.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 /** classes **/
 export { Naro } from "./base/Naro";
-export { NaroId } from "./utils/IdGenerator";
 
 /** Types **/
 export { NaroDocument } from "./base/interfaces/NaroDocument.interface";
