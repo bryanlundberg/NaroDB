26fb89df45cf28a6fc05e2c625bd8a512d33c57a
Bryan Lundberg <bryanlundberg@outlook.com>
2025-04-12 15:07:11 -0600

    refactor: simplify test cases and enhance code readability

--- __test__/unit/base/Naro.test.ts
+++ __test__/unit/base/Naro.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { expect, test } from "vitest";
 import { Naro } from "../../../src";
 import { faker } from "@faker-js/faker/locale/en";
 import { Core } from "../../../src/core/Core";
@@ -6,176 +6,197 @@ import { remove } from "fs-jetpack";
 
 const root = "temp";
 
-describe("Naro", () => {
-  test("add, should add a new document to the users collection", async () => {
-    const db = new Naro(root);
-    const newUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    expect(newUser).toHaveProperty("id");
+test("add, should add a new document to the users collection", async () => {
+  const db = new Naro(root);
+  const newUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  expect(newUser).toHaveProperty("id");
+});
+
+test("getAll, should return an empty array if collection is empty", async () => {
+  const db = new Naro(root);
+  const users = await db.getAll("users");
+  expect(users).toHaveLength(0);
+});
+
+test("getAll, should return all documents in the users collection (with data)", async () => {
+  const db = new Naro(root);
+  for (let i = 0; i < 5; i++) {
+    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  }
+  const users = await db.getAll("users");
+  expect(users).toHaveLength(5);
+});
+
+test("getAll, should return all documents in the users collection (with filter ==)", async () => {
+  const db = new Naro(root);
+
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+
+  const users = await db.getAll("users", { filters: [{ field: "age", operator: "==", value: 30 }] });
+  expect(users).toHaveLength(1);
+});
+
+test("getAll, should return all documents in the users collection (with filter !=)", async () => {
+  const db = new Naro(root);
+
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+
+  const users = await db.getAll("users", { filters: [{ field: "age", operator: "!=", value: 30 }] });
+  expect(users).toHaveLength(2);
+});
+
+test("getAll, should return all documents in the users collection (with filter <)", async () => {
+  const db = new Naro(root);
+
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+
+  const users = await db.getAll("users", { filters: [{ field: "age", operator: "<", value: 30 }] });
+  expect(users).toHaveLength(0);
+});
+
+test("getAll, should return all documents in the users collection (with filter <=)", async () => {
+  const db = new Naro(root);
+
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+
+  const users = await db.getAll("users", { filters: [{ field: "age", operator: "<=", value: 30 }] });
+  expect(users).toHaveLength(1);
+});
+
+test("getAll, should return all documents in the users collection (with filter >)", async () => {
+  const db = new Naro(root);
+
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+
+  const users = await db.getAll("users", { filters: [{ field: "age", operator: ">", value: 30 }] });
+  expect(users).toHaveLength(0);
+});
+
+test("getAll, should return all documents in the users collection (with filter >=)", async () => {
+  const db = new Naro(root);
+
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+
+  const users = await db.getAll("users", { filters: [{ field: "age", operator: ">=", value: 30 }] });
+  expect(users).toHaveLength(1);
+});
+
+test("getAll, should return all documents in the users collection (with limit)", async () => {
+  const db = new Naro(root);
+  for (let i = 0; i < 5; i++) {
+    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  }
+  const users = await db.getAll("users", { limit: 2 });
+  expect(users).toHaveLength(2);
+});
+
+test("getAll, should return all documents in the users collection (with populate)", async () => {
+  const db = new Naro(root);
+  const user = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  await db.add("products", {
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    owner: `users/${user.id}`
   });
-  test("getAll, should return an empty array if collection is empty", async () => {
-    const db = new Naro(root);
-    const users = await db.getAll("users");
-    expect(users).toHaveLength(0);
+  const products = await db.getAll("products", {
+    populate: ["owner"]
   });
-  test("getAll, should return all documents in the users collection (with data)", async () => {
-    const db = new Naro(root);
-    for (let i = 0; i < 5; i++) {
-      await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    }
-    const users = await db.getAll("users");
-    expect(users).toHaveLength(5);
-  });
-  test("getAll, should return all documents in the users collection (with filter ==)", async () => {
-    const db = new Naro(root);
-
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-
-    const users = await db.getAll("users", { filters: [{ field: "age", operator: "==", value: 30 }] });
-    expect(users).toHaveLength(1);
-  });
-  test("getAll, should return all documents in the users collection (with filter !=)", async () => {
-    const db = new Naro(root);
-
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-
-    const users = await db.getAll("users", { filters: [{ field: "age", operator: "!=", value: 30 }] });
-    expect(users).toHaveLength(2);
-  });
-  test("getAll, should return all documents in the users collection (with filter <)", async () => {
-    const db = new Naro(root);
-
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-
-    const users = await db.getAll("users", { filters: [{ field: "age", operator: "<", value: 30 }] });
-    expect(users).toHaveLength(0);
-  });
-  test("getAll, should return all documents in the users collection (with filter <=)", async () => {
-    const db = new Naro(root);
-
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-
-    const users = await db.getAll("users", { filters: [{ field: "age", operator: "<=", value: 30 }] });
-    expect(users).toHaveLength(1);
-  });
-  test("getAll, should return all documents in the users collection (with filter >)", async () => {
-    const db = new Naro(root);
-
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-
-    const users = await db.getAll("users", { filters: [{ field: "age", operator: ">", value: 30 }] });
-    expect(users).toHaveLength(0);
-  });
-  test("getAll, should return all documents in the users collection (with filter >=)", async () => {
-    const db = new Naro(root);
-
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number(), age: 30 });
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-
-    const users = await db.getAll("users", { filters: [{ field: "age", operator: ">=", value: 30 }] });
-    expect(users).toHaveLength(1);
-  });
-  test("getAll, should return all documents in the users collection (with limit)", async () => {
-    const db = new Naro(root);
-    for (let i = 0; i < 5; i++) {
-      await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    }
-    const users = await db.getAll("users", { limit: 2 });
-    expect(users).toHaveLength(2);
-  });
-  test("getAll, should return all documents in the users collection (with populate)", async () => {
-    const db = new Naro(root);
-    const user = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    await db.add("products", { name: faker.commerce.productName(), price: faker.commerce.price(), owner: `users/${user.id}` });
-    const products = await db.getAll("products", {
-      populate: ["owner"]
-    });
-    expect(products).toEqual([
-      {
+  expect(products).toEqual([
+    {
+      name: expect.any(String),
+      price: expect.any(String),
+      owner: {
         name: expect.any(String),
-        price: expect.any(String),
-        owner: {
-          name: expect.any(String),
-          phone: expect.any(String),
-          id: expect.any(String),
-          createdAt: expect.any(Number),
-          path: expect.any(String),
-        },
+        phone: expect.any(String),
         id: expect.any(String),
         createdAt: expect.any(Number),
-        path: expect.any(String),
-      }
-    ]);
-  });
-  test("get, should return a document from the users collection", async () => {
-    const db = new Naro(root);
-    const newUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    const user = await db.get(`users/${newUser.id}`);
-    expect(user).toHaveProperty("id");
-  });
-  test("get, should return undefined if document is not found", async () => {
-    const db = new Naro(root);
-    const user = await db.get("users/123");
-    expect(user).toBeUndefined();
-  });
-  test("update, should update a document in the users collection", async () => {
-    const db = new Naro(root);
-    const initialUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    const afterUser = await db.update(`users/${initialUser.id}`, { name: "John Doe" });
-    expect(afterUser).toHaveProperty("name", "John Doe");
-  });
-  test("update, should throw an error if document is not found", async () => {
-    const db = new Naro(root);
-    try {
-      await db.update("users/123", { name: "John Doe" });
-    } catch (error) {
-      expect(error).toHaveProperty("message", "Item not found");
+        path: expect.any(String)
+      },
+      id: expect.any(String),
+      createdAt: expect.any(Number),
+      path: expect.any(String)
     }
-  });
-  test("delete, should delete a document from the users collection", async () => {
-    const db = new Naro(root);
-    const newUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    expect(await db.getAll("users")).toHaveLength(1);
-    await db.delete(`users/${newUser.id}`);
-    expect(await db.getAll("users")).toHaveLength(0);
-  });
-  test("delete, should silently ignore if document is not found", async () => {
-    const db = new Naro(root);
-    await db.delete("users/123");
-    expect(await db.getAll("users")).toHaveLength(0);
-  });
-  test("writeToDisk, should write a collection to disk", async () => {
-    remove(`./data`);
-    const db = new Naro(root);
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    expect(async () => await db.writeToDisk()).not.toThrow();
-    const core = new Core("./data/"+root);
-    await core.initialize();
-    const collections = await core.loadCollections();
-    expect(collections).toEqual({ users: expect.anything() });
-  });
-  test("writeToDisk, should write two collections to disk", async () => {
-    remove(`./data`);
-    const db = new Naro(root);
-    await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
-    await db.add("products", { name: faker.commerce.productName(), price: faker.commerce.price() });
-    expect(async () => await db.writeToDisk()).not.toThrow();
-    const core = new Core("./data/"+root);
-    await core.initialize();
-    const collections = await core.loadCollections();
-    expect(collections).toEqual({
-      users: expect.anything(),
-      products: expect.anything()
-    });
+  ]);
+});
+
+test("get, should return a document from the users collection", async () => {
+  const db = new Naro(root);
+  const newUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  const user = await db.get(`users/${newUser.id}`);
+  expect(user).toHaveProperty("id");
+});
+
+test("get, should return undefined if document is not found", async () => {
+  const db = new Naro(root);
+  const user = await db.get("users/123");
+  expect(user).toBeUndefined();
+});
+
+test("update, should update a document in the users collection", async () => {
+  const db = new Naro(root);
+  const initialUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  const afterUser = await db.update(`users/${initialUser.id}`, { name: "John Doe" });
+  expect(afterUser).toHaveProperty("name", "John Doe");
+});
+
+test("update, should throw an error if document is not found", async () => {
+  const db = new Naro(root);
+  try {
+    await db.update("users/123", { name: "John Doe" });
+  } catch (error) {
+    expect(error).toHaveProperty("message", "Item not found");
+  }
+});
+
+test("delete, should delete a document from the users collection", async () => {
+  const db = new Naro(root);
+  const newUser = await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  expect(await db.getAll("users")).toHaveLength(1);
+  await db.delete(`users/${newUser.id}`);
+  expect(await db.getAll("users")).toHaveLength(0);
+});
+
+test("delete, should silently ignore if document is not found", async () => {
+  const db = new Naro(root);
+  await db.delete("users/123");
+  expect(await db.getAll("users")).toHaveLength(0);
+});
+
+test("writeToDisk, should write a single collection to disk", async () => {
+  remove(`./data`);
+  const db = new Naro(root);
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  expect(async () => await db.writeToDisk()).not.toThrow();
+  const core = new Core("./data/" + root);
+  await core.initialize();
+  const collections = await core.loadCollections();
+  expect(collections).toEqual({ users: expect.anything() });
+});
+
+test("writeToDisk, should write multiple collections to disk", async () => {
+  remove(`./data`);
+  const db = new Naro(root);
+  await db.add("users", { name: faker.person.fullName(), phone: faker.phone.number() });
+  await db.add("products", { name: faker.commerce.productName(), price: faker.commerce.price() });
+  expect(async () => await db.writeToDisk()).not.toThrow();
+  const core = new Core("./data/" + root);
+  await core.initialize();
+  const collections = await core.loadCollections();
+  expect(collections).toEqual({
+    users: expect.anything(),
+    products: expect.anything()
   });
 });
+
--- __test__/unit/core/Core.test.ts
+++ __test__/unit/core/Core.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, test } from "vitest";
+import { afterEach, beforeEach, expect, test } from "vitest";
 import { Core } from "../../../src/core/Core";
 import { COLLECTION_NAME, DIRNAME_MOCK, FILENAME_MOCK, USERS_MOCK } from "../constants-test";
 import { remove } from "fs-jetpack";
@@ -7,69 +7,65 @@ import { NaroFiler } from "../../../src/manage/files/NaroFiler";
 beforeEach(() => remove(DIRNAME_MOCK), 1000);
 afterEach(() => remove(DIRNAME_MOCK), 1000);
 
-describe("Core", () => {
-  const createFilePath = (collectionSuffix: string) =>
-    `${DIRNAME_MOCK}/${COLLECTION_NAME}${collectionSuffix}/${FILENAME_MOCK}`;
+const createFilePath = (collectionSuffix: string) =>
+  `${DIRNAME_MOCK}/${COLLECTION_NAME}${collectionSuffix}/${FILENAME_MOCK}`;
 
-  test("loadCollections should load data from all existing base collections", async () => {
-    const files = [
-      { path: createFilePath("1"), data: [...USERS_MOCK] },
-      { path: createFilePath("2"), data: [USERS_MOCK[0], USERS_MOCK[2]] }
-    ];
+test("loadCollections should load data from all existing base collections", async () => {
+  const files = [
+    { path: createFilePath("1"), data: [...USERS_MOCK] },
+    { path: createFilePath("2"), data: [USERS_MOCK[0], USERS_MOCK[2]] }
+  ];
 
-    for (const { path, data } of files) {
-      await NaroFiler.writeBinaryFile(path, data);
-    }
-
-    const core = new Core(DIRNAME_MOCK);
-    await core.initialize();
-
-    expect(core.getStructuredCollections()).toEqual({
-      [COLLECTION_NAME + "1"]: files[0].data,
-      [COLLECTION_NAME + "2"]: files[1].data
-    });
-
-  });
-
-  test("getStructuredCollections should return an empty object if the database is empty", () => {
-    const core = new Core(DIRNAME_MOCK);
-    expect(core.getStructuredCollections()).toEqual({});
-  });
-
-  test("writeCollections should write all collections to disk", async () => {
-    const files = [
-      { path: createFilePath("1"), data: [...USERS_MOCK] },
-      { path: createFilePath("2"), data: [USERS_MOCK[0], USERS_MOCK[2]] }
-    ];
-
-    for (const { path, data } of files) {
-      await NaroFiler.writeBinaryFile(path, data);
-    }
-
-    const core = new Core(DIRNAME_MOCK);
-    await core.writeCollections();
-
-    for (const { path, data } of files) {
-      const decodedData = await NaroFiler.readBinaryFile(path);
-      expect(decodedData).toEqual(data);
-    }
-  });
-
-  test("getCollection should return an empty array", async () => {
-    const core = new Core(DIRNAME_MOCK);
-    await core.initialize();
-    expect(core.getCollection("non-existing-collection")).toEqual([]);
-  });
-
-  test("getCollection should return an existing collection", async () => {
-    const data = [...USERS_MOCK];
-    const path = `${DIRNAME_MOCK}/${COLLECTION_NAME}/${FILENAME_MOCK}`;
-
+  for (const { path, data } of files) {
     await NaroFiler.writeBinaryFile(path, data);
+  }
 
-    const core = new Core(DIRNAME_MOCK);
-    await core.initialize();
-    expect(core.getCollection(COLLECTION_NAME)).toStrictEqual(data);
+  const core = new Core(DIRNAME_MOCK);
+  await core.initialize();
+
+  expect(core.getStructuredCollections()).toEqual({
+    [COLLECTION_NAME + "1"]: files[0].data,
+    [COLLECTION_NAME + "2"]: files[1].data
   });
+});
 
+test("getStructuredCollections should return an empty object if the database is empty", () => {
+  const core = new Core(DIRNAME_MOCK);
+  expect(core.getStructuredCollections()).toEqual({});
+});
+
+test("writeCollections should write all collections to disk", async () => {
+  const files = [
+    { path: createFilePath("1"), data: [...USERS_MOCK] },
+    { path: createFilePath("2"), data: [USERS_MOCK[0], USERS_MOCK[2]] }
+  ];
+
+  for (const { path, data } of files) {
+    await NaroFiler.writeBinaryFile(path, data);
+  }
+
+  const core = new Core(DIRNAME_MOCK);
+  await core.writeCollections();
+
+  for (const { path, data } of files) {
+    const decodedData = await NaroFiler.readBinaryFile(path);
+    expect(decodedData).toEqual(data);
+  }
+});
+
+test("getCollection should return an empty array", async () => {
+  const core = new Core(DIRNAME_MOCK);
+  await core.initialize();
+  expect(core.getCollection("non-existing-collection")).toEqual([]);
+});
+
+test("getCollection should return an existing collection", async () => {
+  const data = [...USERS_MOCK];
+  const path = `${DIRNAME_MOCK}/${COLLECTION_NAME}/${FILENAME_MOCK}`;
+
+  await NaroFiler.writeBinaryFile(path, data);
+
+  const core = new Core(DIRNAME_MOCK);
+  await core.initialize();
+  expect(core.getCollection(COLLECTION_NAME)).toStrictEqual(data);
 });
