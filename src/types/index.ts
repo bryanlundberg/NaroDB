d77014d7f5dd6bb89dca184f56003a7c0b47c3ca
Bryan Lundberg <bryanlundberg@outlook.com>
2025-05-17 11:14:39 -0600

    feat: Add centralized export for types in index.ts
    Create a new `src/types/index.ts` to centralize exports for interfaces and types. This simplifies imports across the project and improves code organization.

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export * from './NaroDocument.interface';
+export * from './Options.interface';
+export * from './Query.interface';
+export * from './DocData.interface';
+export * from './Limit.type';
+export * from './Operator.type';
